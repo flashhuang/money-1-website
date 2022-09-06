17f9f2c21d8f0e8a1ec932ae9a46523a7c4c35e1
flashhuang <huang19990915@163.com>
2022-09-06 21:50:53 +0800

    init

---
+++ precache-manifest.f16532c8b52be3cfe7072b623b8cc4a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddbe7bec9e70297d180adc8bc6921c2",
+    "url": "/money-1-website/index.html"
+  },
+  {
+    "revision": "ee12eed075b00672d496",
+    "url": "/money-1-website/static/css/main.47f2af02.chunk.css"
+  },
+  {
+    "revision": "fd6a63f0ab619e48ac85",
+    "url": "/money-1-website/static/js/2.042f04ee.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/money-1-website/static/js/2.042f04ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee12eed075b00672d496",
+    "url": "/money-1-website/static/js/main.38eeff44.chunk.js"
+  },
+  {
+    "revision": "54440bc11e4040c05947",
+    "url": "/money-1-website/static/js/runtime-main.9106bb47.js"
+  }
+]);
